3fd11a4bed2bd5906a9b3a14ace35a3bc681d130
Gonçalo Valentim Vieira Correia <thirtytech@outlook.com>
2020-05-09 22:23:15 +0000

    added plugin model

---
+++ MagmaPlayground_BackEnd/Model/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MagmaPlayground_BackEnd.Model
+{
+    public class Plugin
+    {
+        private int Id;
+        private int order;
+        private Plugintype pluginType;
+
+        public Plugin()
+        {
+
+        }
+    }
+}
---
+++ MagmaPlayground_BackEnd/Model/Plugintype.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MagmaPlayground_BackEnd.Model
+{
+    public enum Plugintype
+    {
+        SAMPLE,
+        INSTRUMENT,
+        AUDIO_EFFECT
+    }
+}
