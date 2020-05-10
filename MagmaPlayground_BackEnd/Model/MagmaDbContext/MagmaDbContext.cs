289be13e927d302f92ed7aec347a59b05d2e4f99
Gonçalo Valentim Vieira Correia <thirtytech@outlook.com>
2020-05-10 14:14:18 +0000

    added MagmaDbContext

---
+++ MagmaPlayground_BackEnd/Model/MagmaDbContext/MagmaDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagmaPlayground_BackEnd.Model.MagmaDbContext
+{
+    public class MagmaDbContext : DbContext
+    {
+        public MagmaDbContext(DbContextOptions<MagmaDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+        public DbSet<Rack> Racks { get; set; }
+        public DbSet<Plugin> Plugins { get; set; }
+        public DbSet<Sampler> Samplers { get; set; }
+        public DbSet<Synthesizer> Synthesizers { get; set; }
+        public DbSet<AudioEffect> AudioEffects { get; set; }
+
+    }
+}
