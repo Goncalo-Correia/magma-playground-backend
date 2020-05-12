df3c1bbb37b12a9a0677117dee7efe2ec9fc8e95
Gonçalo Valentim Vieira Correia <thirtytech@outlook.com>
2020-05-12 21:11:36 +0000

    added test http get method

--- MagmaPlayground_BackEnd/Controllers/HomeController.cs
+++ MagmaPlayground_BackEnd/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MagmaPlayground_BackEnd.Model;
+using MagmaPlayground_BackEnd.Model.MagmaDbContext;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,10 +9,16 @@ namespace MagmaPlayground_BackEnd.Controllers
     [Route("magma_api/{controller}")]
     public class HomeController : Controller
     {
-        [HttpPost]
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult GetHome()
         {
-            return Ok("Home controller: OK");
+            return Ok("Home controller: GET OK");
+        }
+        
+        [HttpPost]
+        public IActionResult PostHome()
+        {
+            return Ok("Home controller: POST Ok");
         }
     }
 }
