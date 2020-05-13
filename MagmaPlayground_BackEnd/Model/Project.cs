99c05d8131981c517c8bc04dbe20339360ba30d2
Gonçalo Valentim Vieira Correia <thirtytech@outlook.com>
2020-05-13 19:44:19 +0000

    added table annotations

--- MagmaPlayground_BackEnd/Model/Project.cs
+++ MagmaPlayground_BackEnd/Model/Project.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace MagmaPlayground_BackEnd.Model
 {
+    [Table("project")]
     public class Project
     {
         [Key]
--- MagmaPlayground_BackEnd/Model/User.cs
+++ MagmaPlayground_BackEnd/Model/User.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace MagmaPlayground_BackEnd.Model
 {
+    [Table("magma_user")]
     public class User
     {
         [Key]
