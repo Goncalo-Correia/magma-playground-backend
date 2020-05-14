d215dda12e440470f9d3807c87722af1690d2a01
Gonçalo Valentim Vieira Correia <thirtytech@outlook.com>
2020-05-14 23:01:36 +0000

    created home controller: login and register methods

--- MagmaPlayground_BackEnd/Controllers/HomeController.cs
+++ MagmaPlayground_BackEnd/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using MagmaPlayground_BackEnd.Model;
 using MagmaPlayground_BackEnd.Model.MagmaDbContext;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,21 +12,42 @@ namespace MagmaPlayground_BackEnd.Controllers
     [Route("magma_api/[controller]")]
     public class HomeController : ControllerBase
     {
+        public UserController userController;
+
         public MagmaDbContext magmaDbContext;
+        public ActionResult<User> user;
 
         public HomeController(MagmaDbContext magmaDbContext)
-            => this.magmaDbContext = magmaDbContext;
+        {
+            userController = new UserController(magmaDbContext);
+            this.magmaDbContext = magmaDbContext;
+        }
+            
         
         [HttpGet]
-        public ActionResult Login()
+        public ActionResult Login(string email, string password)
         {
-            return Ok("Success: get request");
+            user = userController.GetUserByEmail(email);
+            if(user.Value.password == password)
+            {
+                return Ok("Success: login valid");
+            }
+
+            return NotFound("Error: user not found");
         }
         
         [HttpPost]
-        public ActionResult Register()
+        public ActionResult Register(User registerUser)
         {
-            return Ok("Success: post request");
+            user = userController.GetUserByEmail(registerUser.email);
+            if(user.Value.id == 0)
+            {
+                userController.CreateUser(registerUser);
+
+                return Ok("Success: register valid");
+            }
+
+            return BadRequest("Error: user already exists");
         }
     }
 }
