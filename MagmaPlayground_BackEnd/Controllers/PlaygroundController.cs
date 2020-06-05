b6bb97236c5b354ab471c0980b867b43486b34be
Gonçalo Valentim Vieira Correia <thirtytech@outlook.com>
2020-06-05 00:14:25 +0000

    started playground service and playground controller

---
+++ MagmaPlayground_BackEnd/Controllers/PlaygroundController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagmaPlayground_BackEnd.Controllers
+{
+    public class PlaygroundController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
