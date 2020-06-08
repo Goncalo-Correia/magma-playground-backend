891c9b5e5d62c3ebe6f30b87b4c02ac8a8be9409
Gonçalo Valentim Vieira Correia <thirtytech@outlook.com>
2020-06-08 22:40:04 +0000

    added services and daos skeleton

--- MagmaPlayground_BackEnd/Services/PlaygroundService.cs
+++ MagmaPlayground_BackEnd/Services/PlaygroundService.cs
@@ -12,38 +12,6 @@ namespace MagmaPlayground_BackEnd.Services
 {
     public class PlaygroundService
     {
-        private MagmaDbContext magmaDbContext;
-
-        private UserController userController;
-        private ProjectController projectController;
-        private TrackController trackController;
-        private RackController rackController;
-        private PluginController pluginController;
-        private SamplerController samplerController;
-        private SynthesizerController synthesizerController;
-        private AudioEffectController audioEffectController;
-
-        private TrackFactory trackFactory;
-
-        public PlaygroundService (MagmaDbContext magmaDbContext)
-        {
-            this.magmaDbContext = magmaDbContext;
-
-            this.userController = new UserController(magmaDbContext);
-            this.projectController = new ProjectController(magmaDbContext);
-            this.trackController = new TrackController(magmaDbContext);
-            this.rackController = new RackController(magmaDbContext);
-            this.pluginController = new PluginController(magmaDbContext);
-            this.samplerController = new SamplerController(magmaDbContext);
-            this.synthesizerController = new SynthesizerController(magmaDbContext);
-            this.audioEffectController = new AudioEffectController(magmaDbContext);
-
-            this.trackFactory = new TrackFactory(projectController, trackController, rackController);
-        }
-
-        public void CreateDefaultProject(int userId)
-        {
-
-        }
+        
     }
 }
