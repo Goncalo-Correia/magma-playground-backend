0ccd6aecbb277d696aed842a71234c604b3ae7a2
Gonçalo Valentim Vieira Correia <thirtytech@outlook.com>
2020-06-23 20:12:21 +0000

    added rack service

--- MagmaPlayground_BackEnd/Daos/RackDao.cs
+++ MagmaPlayground_BackEnd/Daos/RackDao.cs
@@ -21,141 +21,46 @@ namespace MagmaPlayground_BackEnd.Daos
             this.responseFactory = new ResponseFactory();
         }
 
-        public Response GetRack(int id)
+        public Response GetRackById(int id)
         {
             response = new Response();
-
-            if (id == 0)
-            {
-                return responseFactory.BuildResponse("Error: input parameter is null", ResponseStatus.BADREQUEST);
-            }
 
             response.rack = magmaDbContext.Find<Rack>(id);
             response.message = "Success: found track";
             response.responseStatus = ResponseStatus.OK;
-
-            if (response.rack == null)
-            {
-                return responseFactory.BuildResponse("Error: rack not found", ResponseStatus.NOTFOUND);
-            }
 
             return response;
         }
 
         public Response GetRackByTrackId(int trackId)
         {
-            try
-            {
-                if (trackId == 0)
-                {
-                    return responseFactory.BuildResponse("Error: input parameter is null", ResponseStatus.BADREQUEST);
-                }
-
-                response.rack = magmaDbContext.Racks.Single<Rack>(prop => prop.trackId == trackId);
-                response.message = "Success: rack found";
-                response.responseStatus = ResponseStatus.OK;
-
-                if (response.rack == null)
-                {
-                    return responseFactory.BuildResponse("Error: rack not found for this track", ResponseStatus.NOTFOUND);
-                }
-            }
-            catch (ArgumentNullException ex)
-            {
-                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
-            }
-            catch (InvalidOperationException ex)
-            {
-                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
-            }
+            response.rack = magmaDbContext.Racks.Single<Rack>(prop => prop.trackId == trackId);
+            response.message = "Success: rack found";
+            response.responseStatus = ResponseStatus.OK;
 
             return response;
         }
 
         public Response CreateRack(Rack rack)
         {
-            try
-            {
-                if (rack == null)
-                {
-                    return responseFactory.BuildResponse("Error: input parameter is null", ResponseStatus.BADREQUEST);
-                }
-
-                if (rack.id != 0)
-                {
-                    return responseFactory.BuildResponse("Error: rack already exists, id must be null", ResponseStatus.BADREQUEST);
-                }
-
-                magmaDbContext.Add<Rack>(rack);
-                magmaDbContext.SaveChanges();
-
-            }
-            catch (DbUpdateConcurrencyException ex)
-            {
-                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
-            }
-            catch (DbUpdateException ex)
-            {
-                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
-            }
+            magmaDbContext.Add<Rack>(rack);
+            magmaDbContext.SaveChanges();
 
             return responseFactory.BuildResponse("Success: created rack", ResponseStatus.OK);
         }
 
         public Response UpdateRack(Rack rack)
         {
-            try
-            {
-                if (rack == null)
-                {
-                    return responseFactory.BuildResponse("Error: input parameter is null", ResponseStatus.BADREQUEST);
-                }
-
-                if (rack.id == 0)
-                {
-                    return responseFactory.BuildResponse("Error: rack id is null", ResponseStatus.BADREQUEST);
-                }
-
-                magmaDbContext.Update<Rack>(rack);
-                magmaDbContext.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException ex)
-            {
-                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
-            }
-            catch (DbUpdateException ex)
-            {
-                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
-            }
+            magmaDbContext.Update<Rack>(rack);
+            magmaDbContext.SaveChanges();
 
             return responseFactory.BuildResponse("Success: updated rack", ResponseStatus.OK);
         }
 
         public Response DeleteRack(Rack rack)
         {
-            try
-            {
-                if (rack == null)
-                {
-                    return responseFactory.BuildResponse("Error: input parameter is null", ResponseStatus.BADREQUEST);
-                }
-
-                if (rack.id == 0)
-                {
-                    return responseFactory.BuildResponse("Error: rack id is null", ResponseStatus.BADREQUEST);
-                }
-
-                magmaDbContext.Remove<Rack>(rack);
-                magmaDbContext.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException ex)
-            {
-                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
-            }
-            catch (DbUpdateException ex)
-            {
-                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
-            }
+            magmaDbContext.Remove<Rack>(rack);
+            magmaDbContext.SaveChanges();
 
             return responseFactory.BuildResponse("Success: removed rack", ResponseStatus.OK);
         }
--- MagmaPlayground_BackEnd/Services/RackService.cs
+++ MagmaPlayground_BackEnd/Services/RackService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using MagmaPlayground_BackEnd.Daos;
+using MagmaPlayground_BackEnd.Model;
+using MagmaPlayground_BackEnd.Model.MagmaDbContext;
+using MagmaPlayground_BackEnd.ResponseUtilities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +12,149 @@ namespace MagmaPlayground_BackEnd.Services
 {
     public class RackService
     {
+        private RackDao rackDao;
+        private ResponseFactory responseFactory;
+        private Response response;
+
+        public RackService(MagmaDbContext magmaDbContext)
+        {
+            rackDao = new RackDao(magmaDbContext);
+            responseFactory = new ResponseFactory();
+        }
+
+        public Response GetRackById(int id)
+        {
+            response = new Response();
+
+            if (id == 0)
+            {
+                return responseFactory.BuildResponse("Error: input parameter is null", ResponseStatus.BADREQUEST);
+            }
+
+            response = rackDao.GetRackById(id);
+
+            if (response.rack == null)
+            {
+                return responseFactory.BuildResponse("Error: rack not found", ResponseStatus.NOTFOUND);
+            }
+
+            return response;
+        }
+
+        public Response GetRackByTrackId(int trackId)
+        {
+            response = new Response();
+            try
+            {
+                if (trackId == 0)
+                {
+                    return responseFactory.BuildResponse("Error: input parameter is null", ResponseStatus.BADREQUEST);
+                }
+
+                response = rackDao.GetRackByTrackId(trackId);
+
+                if (response.rack == null)
+                {
+                    return responseFactory.BuildResponse("Error: rack not found for this track", ResponseStatus.NOTFOUND);
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
+            }
+
+            return response;
+        }
+
+        public Response CreateRack(Rack rack)
+        {
+            response = new Response();
+            try
+            {
+                if (rack == null)
+                {
+                    return responseFactory.BuildResponse("Error: input parameter is null", ResponseStatus.BADREQUEST);
+                }
+
+                if (rack.id != 0)
+                {
+                    return responseFactory.BuildResponse("Error: rack already exists, id must be null", ResponseStatus.BADREQUEST);
+                }
+
+                response = rackDao.CreateRack(rack);
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
+            }
+            catch (DbUpdateException ex)
+            {
+                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
+            }
+
+            return response;
+        }
+
+        public Response UpdateRack(Rack rack)
+        {
+            response = new Response();
+            try
+            {
+                if (rack == null)
+                {
+                    return responseFactory.BuildResponse("Error: input parameter is null", ResponseStatus.BADREQUEST);
+                }
+
+                if (rack.id == 0)
+                {
+                    return responseFactory.BuildResponse("Error: rack id is null", ResponseStatus.BADREQUEST);
+                }
+
+                response = rackDao.UpdateRack(rack);
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
+            }
+            catch (DbUpdateException ex)
+            {
+                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
+            }
+
+            return response;
+        }
+
+        public Response DeleteRack(Rack rack)
+        {
+            response = new Response();
+            try
+            {
+                if (rack == null)
+                {
+                    return responseFactory.BuildResponse("Error: input parameter is null", ResponseStatus.BADREQUEST);
+                }
+
+                if (rack.id == 0)
+                {
+                    return responseFactory.BuildResponse("Error: rack id is null", ResponseStatus.BADREQUEST);
+                }
+
+                response = rackDao.DeleteRack(rack);
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
+            }
+            catch (DbUpdateException ex)
+            {
+                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
+            }
+
+            return response;
+        }
     }
 }
