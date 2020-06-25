a8b7656154ef1984af083498df24a4f49854ee7c
Gonçalo Valentim Vieira Correia <thirtytech@outlook.com>
2020-06-25 18:51:21 +0000

    added synthesizer service

--- MagmaPlayground_BackEnd/Daos/SynthesizerDao.cs
+++ MagmaPlayground_BackEnd/Daos/SynthesizerDao.cs
@@ -21,140 +21,48 @@ namespace MagmaPlayground_BackEnd.Daos
             this.responseFactory = new ResponseFactory();
         }
 
-        public Response GetSynthesizer(int id)
+        public Response GetSynthesizerById(int id)
         {
             response = new Response();
-
-            if (id == 0)
-            {
-                return responseFactory.BuildResponse("Error: input parameter is null", ResponseStatus.BADREQUEST);
-            }
 
             response.synthesizer = magmaDbContext.Find<Synthesizer>(id);
             response.message = "Success: synthesizer found";
             response.responseStatus = ResponseStatus.OK;
-
-            if (response.synthesizer == null)
-            {
-                return responseFactory.BuildResponse("Error: synthesizer not found", ResponseStatus.NOTFOUND);
-            }
 
             return response;
         }
 
         public Response GetSynthesizerByPluginId(int pluginId)
         {
-            try
-            {
-                if (pluginId == 0)
-                {
-                    return responseFactory.BuildResponse("Error: input parameter is null", ResponseStatus.BADREQUEST);
-                }
+            response = new Response();
 
-                response.synthesizer = magmaDbContext.Synthesizers.Single<Synthesizer>(prop => prop.plugin.id == pluginId);
-                response.message = "Success: synthesizer found";
-                response.responseStatus = ResponseStatus.OK;
-
-                if (response.synthesizer == null)
-                {
-                    return responseFactory.BuildResponse("Error: synthesizer not found for this plugin", ResponseStatus.NOTFOUND);
-                }
-            }
-            catch (ArgumentNullException ex)
-            {
-                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
-            }
-            catch (InvalidOperationException ex)
-            {
-                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
-            }
+            response.synthesizer = magmaDbContext.Synthesizers.Single<Synthesizer>(prop => prop.plugin.id == pluginId);
+            response.message = "Success: synthesizer found";
+            response.responseStatus = ResponseStatus.OK;
 
             return response;
         }
 
         public Response CreateSynthesizer(Synthesizer synthesizer)
         {
-            try
-            {
-                if (synthesizer == null)
-                {
-                    return responseFactory.BuildResponse("Error: input parameter is null", ResponseStatus.BADREQUEST);
-                }
-
-                if (synthesizer.id != 0)
-                {
-                    return responseFactory.BuildResponse("Error: synthesizer already exists, id must be null", ResponseStatus.BADREQUEST);
-                }
-
-                magmaDbContext.Add<Synthesizer>(synthesizer);
-                magmaDbContext.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException ex)
-            {
-                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
-            }
-            catch (DbUpdateException ex)
-            {
-                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
-            }
+            magmaDbContext.Add<Synthesizer>(synthesizer);
+            magmaDbContext.SaveChanges();
 
             return responseFactory.BuildResponse("Success: created synthesizer", ResponseStatus.OK);
         }
 
         public Response UpdateSynthesizer(Synthesizer synthesizer)
         {
-            try
-            {
-                if (synthesizer == null)
-                {
-                    return responseFactory.BuildResponse("Error: input parameter is null", ResponseStatus.BADREQUEST);
-                }
-
-                if (synthesizer.id == 0)
-                {
-                    return responseFactory.BuildResponse("Error: synthesizer id is null", ResponseStatus.BADREQUEST);
-                }
-
-                magmaDbContext.Update<Synthesizer>(synthesizer);
-                magmaDbContext.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException ex)
-            {
-                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
-            }
-            catch (DbUpdateException ex)
-            {
-                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
-            }
+            magmaDbContext.Update<Synthesizer>(synthesizer);
+            magmaDbContext.SaveChanges();
 
             return responseFactory.BuildResponse("Success: updated synthesizer", ResponseStatus.OK);
         }
 
         public Response DeleteSynthesizer(Synthesizer synthesizer)
         {
-            try
-            {
-                if (synthesizer == null)
-                {
-                    return responseFactory.BuildResponse("Error: input parameter is null", ResponseStatus.BADREQUEST);
-                }
-
-                if (synthesizer.id == 0)
-                {
-                    return responseFactory.BuildResponse("Error: synthesizer id is null", ResponseStatus.BADREQUEST);
-                }
-
                 magmaDbContext.Remove<Synthesizer>(synthesizer);
                 magmaDbContext.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException ex)
-            {
-                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
-            }
-            catch (DbUpdateException ex)
-            {
-                return responseFactory.BuildResponse("exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
-            }
 
             return responseFactory.BuildResponse("Success: removed synthesizer", ResponseStatus.OK);
         }
--- MagmaPlayground_BackEnd/Services/SynthesizerService.cs
+++ MagmaPlayground_BackEnd/Services/SynthesizerService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using MagmaPlayground_BackEnd.Daos;
+using MagmaPlayground_BackEnd.Model;
+using MagmaPlayground_BackEnd.Model.MagmaDbContext;
+using MagmaPlayground_BackEnd.ResponseUtilities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +12,149 @@ namespace MagmaPlayground_BackEnd.Services
 {
     public class SynthesizerService
     {
+        private SynthesizerDao synthesizerDao;
+        private ResponseFactory responseFactory;
+        private Response response;
+
+        public SynthesizerService(MagmaDbContext magmaDbContext)
+        {
+            synthesizerDao = new SynthesizerDao(magmaDbContext);
+            responseFactory = new ResponseFactory();
+        }
+
+        public Response GetSynthesizerById(int id)
+        {
+            response = new Response();
+
+            if (id == 0)
+            {
+                return responseFactory.BuildResponse("Error: input parameter is null", ResponseStatus.BADREQUEST);
+            }
+
+            response = synthesizerDao.GetSynthesizerById(id);
+
+            if (response.synthesizer == null)
+            {
+                return responseFactory.BuildResponse("Error: synthesizer not found", ResponseStatus.NOTFOUND);
+            }
+
+            return response;
+        }
+
+        public Response GetSynthesizerByPluginId(int pluginId)
+        {
+            response = new Response();
+            try
+            {
+                if (pluginId == 0)
+                {
+                    return responseFactory.BuildResponse("Error: input parameter is null", ResponseStatus.BADREQUEST);
+                }
+
+                response = synthesizerDao.GetSynthesizerByPluginId(pluginId);
+
+                if (response.synthesizer == null)
+                {
+                    return responseFactory.BuildResponse("Error: synthesizer not found for this plugin", ResponseStatus.NOTFOUND);
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
+            }
+
+            return response;
+        }
+
+        public Response CreateSynthesizer(Synthesizer synthesizer)
+        {
+            response = new Response();
+            try
+            {
+                if (synthesizer == null)
+                {
+                    return responseFactory.BuildResponse("Error: input parameter is null", ResponseStatus.BADREQUEST);
+                }
+
+                if (synthesizer.id != 0)
+                {
+                    return responseFactory.BuildResponse("Error: synthesizer already exists, id must be null", ResponseStatus.BADREQUEST);
+                }
+
+                response = synthesizerDao.CreateSynthesizer(synthesizer);
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
+            }
+            catch (DbUpdateException ex)
+            {
+                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
+            }
+
+            return response;
+        }
+
+        public Response UpdateSynthesizer(Synthesizer synthesizer)
+        {
+            response = new Response();
+            try
+            {
+                if (synthesizer == null)
+                {
+                    return responseFactory.BuildResponse("Error: input parameter is null", ResponseStatus.BADREQUEST);
+                }
+
+                if (synthesizer.id == 0)
+                {
+                    return responseFactory.BuildResponse("Error: synthesizer id is null", ResponseStatus.BADREQUEST);
+                }
+
+                response = synthesizerDao.UpdateSynthesizer(synthesizer);
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
+            }
+            catch (DbUpdateException ex)
+            {
+                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
+            }
+
+            return response;
+        }
+
+        public Response DeleteSynthesizer(Synthesizer synthesizer)
+        {
+            response = new Response();
+            try
+            {
+                if (synthesizer == null)
+                {
+                    return responseFactory.BuildResponse("Error: input parameter is null", ResponseStatus.BADREQUEST);
+                }
+
+                if (synthesizer.id == 0)
+                {
+                    return responseFactory.BuildResponse("Error: synthesizer id is null", ResponseStatus.BADREQUEST);
+                }
+
+                response = synthesizerDao.DeleteSynthesizer(synthesizer);
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return responseFactory.BuildResponse("Exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
+            }
+            catch (DbUpdateException ex)
+            {
+                return responseFactory.BuildResponse("exception: " + ex.InnerException.Message, ResponseStatus.EXCEPTION);
+            }
+
+            return response;
+        }
     }
 }
