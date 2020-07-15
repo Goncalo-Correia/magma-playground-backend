2fb74241226f99017b840d97bbb650b6735dfaea
Gonçalo Valentim Vieira Correia <thirtytech@outlook.com>
2020-07-15 11:43:26 +0000

    added save project method to playground dao

--- MagmaPlayground_BackEnd/Daos/PlaygroundDao.cs
+++ MagmaPlayground_BackEnd/Daos/PlaygroundDao.cs
@@ -111,7 +111,79 @@ namespace MagmaPlayground_BackEnd.Daos
 
         public Response SaveProject(Project project)
         {
+            int projectId;
+            int trackId;
+            int rackId;
+            int pluginId;
 
+            projectId = projectDao.UpdateProject(project).id;
+
+            foreach (Track track in project.tracks)
+            {
+                if (track.id == 0)
+                {
+                    track.projectId = projectId;
+                    trackId = trackDao.CreateTrack(track).id;
+
+                    track.rack.trackId = trackId;
+                    rackId = rackDao.CreateRack(track.rack).id;
+                } 
+                else
+                {
+                    trackId = trackDao.UpdateTrack(track).id;
+                    rackId = rackDao.UpdateRack(track.rack).id;
+                }
+
+                foreach (Plugin plugin in track.rack.plugins)
+                {
+                    if (plugin.id == 0)
+                    {
+                        plugin.rackId = rackId;
+                        pluginId = pluginDao.CreatePlugin(plugin).id;
+                    }
+                    else
+                    {
+                        pluginId = pluginDao.UpdatePlugin(plugin).id;
+                    }
+
+                    switch (plugin.pluginType)
+                    {
+                        case PluginType.SAMPLER:
+                            if (plugin.id == 0)
+                            {
+                                plugin.sampler.pluginId = pluginId;
+                                samplerDao.CreateSampler(plugin.sampler);
+                            }
+                            else
+                            {
+                                samplerDao.UpdateSampler(plugin.sampler);
+                            }
+                            break;
+                        case PluginType.SYNTHESIZER:
+                            if (plugin.id == 0)
+                            {
+                                plugin.synthesizer.pluginId = pluginId;
+                                synthesizerDao.CreateSynthesizer(plugin.synthesizer);
+                            }
+                            else
+                            {
+                                synthesizerDao.UpdateSynthesizer(plugin.synthesizer);
+                            }
+                            break;
+                        case PluginType.AUDIOEFFECT:
+                            if (plugin.id == 0)
+                            {
+                                plugin.audioEffect.pluginId = pluginId;
+                                audioEffectDao.CreateAudioEffect(plugin.audioEffect);
+                            }
+                            else
+                            {
+                                audioEffectDao.UpdateAudioEffect(plugin.audioEffect);
+                            }
+                            break;
+                    }
+                }
+            }
 
             return responseFactory.BuildResponse("Success: saved project", ResponseStatus.OK);
         }
