dfdcfb85895441cc4425cdeffe565b0eacd38ad8
Gonçalo Valentim Vieira Correia <thirtytech@outlook.com>
2020-08-31 22:53:53 +0000

    added playground controller

--- MagmaPlayground_BackEnd/Controllers/PlaygroundController.cs
+++ MagmaPlayground_BackEnd/Controllers/PlaygroundController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MagmaPlayground_BackEnd.Model;
 using MagmaPlayground_BackEnd.Model.MagmaDbContext;
 using MagmaPlayground_BackEnd.ResponseUtilities;
 using MagmaPlayground_BackEnd.Services;
@@ -28,6 +29,33 @@ namespace MagmaPlayground_BackEnd.Controllers
         {
             response = new Response();
             response = playgroundService.GetProjectById(id);
+
+            return responseFactory.BuildControllerResponse(response);
+        }
+
+        [HttpPost("project/savenew")]
+        public ActionResult<Response> SaveNewProject(Project project)
+        {
+            response = new Response();
+            response = playgroundService.SaveNewProject(project);
+
+            return responseFactory.BuildControllerResponse(response);
+        }
+
+        [HttpPost("project/save")]
+        public ActionResult<Response> SaveProject(Project project)
+        {
+            response = new Response();
+            response = playgroundService.SaveProject(project);
+
+            return responseFactory.BuildControllerResponse(response);
+        }
+
+        [HttpDelete]
+        public ActionResult<Response> DeleteProject(Project project)
+        {
+            response = new Response();
+            response = playgroundService.DeleteProject(project);
 
             return responseFactory.BuildControllerResponse(response);
         }
