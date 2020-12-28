ed42d5d6ffe050893872a7122700d3ea78a54e98
thirtytech <thirtytech@outlook.com>
2020-12-28 00:16:29 -0100

    refactor project structure

---
+++ MagmaPlayground_BackEnd/MagmaLive/Response/LiveResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MagmaPlayground_BackEnd.Models.MagmaLive.Response
+{
+    public class LiveResponse
+    {
+        public Live live { get; set; }
+        public List<Live> lives { get; set; }
+
+        public LiveFile liveFile { get; set; }
+        public List<LiveFile> liveFiles { get; set; }
+
+        public bool isSuccess { get; set; }
+        public string errorMessage { get; set; }
+    }
+}
