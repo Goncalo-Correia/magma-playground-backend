e38721b3cb79ec9f82be30f7bbbc494bdd3a21c4
thirtytech <thirtytech@outlook.com>
2021-02-13 00:02:45 -0100

    refactor magma-daw model cofiguration

--- MagmaPlayground_BackEnd/MagmaDB/MagmaDaw/MagmaDawDbContext.cs
+++ MagmaPlayground_BackEnd/MagmaDB/MagmaDaw/MagmaDawDbContext.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using MagmaPlayground_BackEnd.Model.Configurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace MagmaPlayground_BackEnd.Model.MagmaDbContext
@@ -16,42 +15,10 @@ namespace MagmaPlayground_BackEnd.Model.MagmaDbContext
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.ApplyConfiguration(new UserConfiguration());
-
-            modelBuilder.ApplyConfiguration(new ProjectConfiguration());
-
-            modelBuilder.ApplyConfiguration(new TrackConfiguration());
-
-            modelBuilder.ApplyConfiguration(new RackConfiguration());
-
-            modelBuilder.ApplyConfiguration(new PluginConfiguration());
-
-            modelBuilder.ApplyConfiguration(new SamplerConfiguration());
-
-            modelBuilder.ApplyConfiguration(new SynthesizerConfiguration());
-
-            modelBuilder.ApplyConfiguration(new AudioEffectConfiguration());
-       
-            modelBuilder.Entity<Project>()
-                .HasOne(prop => prop.user)
-                .WithMany(prop => prop.projects)
-                .HasForeignKey(prop => prop.userId)
-                .HasConstraintName("project_user_fkey");
-
-            modelBuilder.Entity<Track>()
-                .HasOne(prop => prop.Project)
-                .WithMany(prop => prop.tracks)
-                .HasForeignKey(prop => prop.projectId)
-                .HasConstraintName("track_project_fkey");
-
-            modelBuilder.Entity<Plugin>()
-                .HasOne(prop => prop.rack)
-                .WithMany(prop => prop.plugins)
-                .HasForeignKey(prop => prop.rackId)
-                .HasConstraintName("plugin_rack_fkey");
+            modelBuilder.HasDefaultSchema("magma_daw");
+            base.OnModelCreating(modelBuilder);
         }
 
-        public DbSet<User> Users { get; set; }
         public DbSet<Project> Projects { get; set; }
         public DbSet<Track> Tracks { get; set; }
         public DbSet<Rack> Racks { get; set; }
